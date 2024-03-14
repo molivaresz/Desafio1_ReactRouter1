bba549f39b0888ac3e110c28c24d2aeaf613cbc9
molivaresz <molivaresz1@gmail.com>
2024-03-13 22:47:45 -0300

    add: include Navbar with router and link

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Routes, Route } from 'react-router'
+import Navbarhappycake from './components/Navbarhappycake'
+import Home from './views/Home'
+import Contact from './views/Contact'
+
+
+function App() {
+
+  return (
+    <>
+    <Container>
+      <Row className='bg-danger'>
+        <Col className='text-light'>
+          <Navbarhappycake/>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+          </Routes>
+        </Col>
+      </Row>
+    </Container>
+    </>
+  )
+}
+
+export default App
